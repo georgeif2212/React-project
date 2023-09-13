c75f3fb7e75bc8d3463eb7ddae9cc0e8598c959a
georgeif2212 <jinfante2212@gmail.com>
2023-09-13 16:48:42 -0600

    Actualización de links navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,33 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { CartWidget } from "./CartWidget";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <>
       <Navbar bg="light" data-bs-theme="light" className="navbar">
         <Container>
-          <Navbar.Brand href="#home">FusionStyleHub</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            FusionStyleHub
+          </Navbar.Brand>
+
           <Nav className="me-auto">
-            <Nav.Link href="/category/men's clothing">Hombres</Nav.Link>
-            <Nav.Link href="/category/women's clothing">Mujeres</Nav.Link>
-            <Nav.Link href="/category/jewelery">Joyería</Nav.Link>
-            <Nav.Link href="/category/electronics">Electrónicos</Nav.Link>
+            <Nav.Link as={Link} to="/category/men's clothing">
+              Hombres
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/category/women's clothing">
+              Mujeres
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/category/jewelery">
+              Joyería
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/category/electronics">
+              Electrónicos
+            </Nav.Link>
           </Nav>
           <CartWidget />
         </Container>
