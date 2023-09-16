139b0f4a6e51496671e1c7ee374ddb1da1db77b3
georgeif2212 <jinfante2212@gmail.com>
2023-09-15 18:18:44 -0600

    Ahora el carrito no se duplica

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,8 +3,27 @@ export const CartContext = createContext([]);
 
 export const CartProvider = ({ children }) => {
   const [items, setItems] = useState([]);
-  const addItem = (producto, quantity) =>
-    setItems((prev) => [...prev, { ...producto, quantity }]);
+  const addItem = (producto, quantity) => {
+    // * Se busca si anteriormente se ha metido un producto al carrito con el mismo id
+    const existe = items.some((item) => item.id === producto.id);
+    // * Si no existe, se agrega al carrito
+    if (!existe) setItems((prev) => [...prev, { ...producto, quantity }]);
+    // * Si existe
+    else {
+      // * Se recorre el array y el producto que está en el array del carrito se
+      // * le suma la cantidad que se le quiere meter
+      const updateItems = items.map((item) => {
+        if (item.id === producto.id) {
+          return {
+            ...item,
+            quantity: item.quantity + quantity,
+          };
+        } else return item;
+      });
+      setItems(updateItems);
+    }
+  };
+
   // const addItem = (producto, quantity) => console.log(producto, quantity);
 
   const totalWidget = items.reduce((acc, val) => acc + val.quantity, 0);
@@ -18,7 +37,9 @@ export const CartProvider = ({ children }) => {
     setItems([]);
   };
   return (
-    <CartContext.Provider value={{ addItem, removeItem, clear, totalWidget, items}}>
+    <CartContext.Provider
+      value={{ addItem, removeItem, clear, totalWidget, items }}
+    >
       {children}
     </CartContext.Provider>
   );
