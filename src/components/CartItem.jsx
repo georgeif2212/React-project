1dbcd054817b77fa42069d4a9201949f1c6380de
georgeif2212 <jinfante2212@gmail.com>
2023-09-18 16:37:17 -0600

    Cambios en los estilos y diseño de cartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,27 +1,34 @@
 export const CartItem = ({ item, addItem, removeItem, clear }) => {
   return (
-    <article key={item.id}>
-      <img
-        style={{ width: "20%", height: "auto" }}
-        className="producto-carrito__img"
-        src={item.imagen}
-        alt=""
-      />
-      <div className="producto-carrito-info">
-        <div>
-          <h4 className="color-1 size-medium_s mb-2">{item.nombre}</h4>
-          <p className="color-2 size-small_l mb-1">{item.description}</p>
-        </div>
-        <h4 className="color-1 size-medium_s mb-2">
-          Cantidad: {item.quantity}
-        </h4>
-        <div className="producto-carrito-info-extra">
-          <p className="color-2 size-medium_s mb-1">
-            <b>
-              $<span>{item.precio}</span>
-            </b>
-          </p>
-          <button onClick={() => removeItem(item.id)}>Eliminar</button>
+    <article className="cartItem" key={item.id}>
+      <img className="cartItem__img" src={item.imagen} alt="" />
+      <div className="cartItem-info">
+        <div style={{ display: "flex" }}>
+          <div>
+            <h2 className="color-1 size-medium_s mb-2 cartItem-info__name">
+              {item.nombre}
+            </h2>
+            <p className="color-2 size-small_l mb-1">{item.description}</p>
+            <div style={{ display: "flex", alignSelf: "end" }}>
+              <p
+                style={{ cursor: "pointer" }}
+                onClick={() => removeItem(item.id)}
+              >
+                Eliminar
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className="color-1 size-medium_s mb-2">
+              Cantidad: {item.quantity}
+            </p>
+
+            <p className="color-2 size-medium_s mb-1">
+              <b>
+                $<span>{item.precio}</span>
+              </b>
+            </p>
+          </div>
         </div>
       </div>
     </article>
